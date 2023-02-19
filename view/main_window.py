eca6862893b1cd11412ce00de65be286eeaf35d4
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2023-02-19 20:51:09 +0100

    catching errors and closing app on 'q' press

--- view/main_window.py
+++ view/main_window.py
@@ -1,38 +1,55 @@
 """creates main window of the application"""
 
 import curses
-from time import sleep
 from math import floor
+
+from .ui import close_ui
 
 
 def draw_main_window(screen) -> None:
 
-  SCREEN_WIDTH = curses.COLS
-  SCREEN_HEIGHT = curses.LINES
+    try:
+        SCREEN_WIDTH = curses.COLS
+        SCREEN_HEIGHT = curses.LINES
 
-  PANEL_WIDTH = floor(SCREEN_WIDTH / 3)
-  PANEL_HEIGHT = floor(SCREEN_HEIGHT / 3)
+        PANEL_WIDTH = floor(SCREEN_WIDTH / 3)
+        PANEL_HEIGHT = floor(SCREEN_HEIGHT / 3)
 
-  list_window = curses.newwin(SCREEN_HEIGHT - PANEL_HEIGHT, PANEL_WIDTH, 0, 0)
-  options_window = curses.newwin(
-      PANEL_HEIGHT, PANEL_WIDTH, SCREEN_HEIGHT-PANEL_HEIGHT, 0)
-  result_window = curses.newwin(
-      SCREEN_HEIGHT, SCREEN_WIDTH-PANEL_WIDTH, 0, PANEL_WIDTH)
+        list_window = curses.newwin(
+            SCREEN_HEIGHT - PANEL_HEIGHT, PANEL_WIDTH, 0, 0)
 
-  list_window.border()
-  options_window.border()
-  result_window.border()
+        options_window = curses.newwin(
+            PANEL_HEIGHT, PANEL_WIDTH, SCREEN_HEIGHT-PANEL_HEIGHT, 0)
 
-  list_window.refresh()
-  options_window.refresh()
-  result_window.refresh()
+        result_window = curses.newwin(
+            SCREEN_HEIGHT, SCREEN_WIDTH-PANEL_WIDTH, 0, PANEL_WIDTH)
 
-  screen.refresh()
+        list_window.border()
+        options_window.border()
+        result_window.border()
 
-  sleep(2)
+        list_window.refresh()
+        options_window.refresh()
+        result_window.refresh()
 
-  del list_window
-  del options_window
-  del result_window
+        screen.refresh()
 
-  screen.refresh()
+        active = True
+
+        while active:
+            input = screen.getkey()
+
+            if input == "q":
+                active = False
+            else:
+                curses.flash()
+
+        del list_window
+        del options_window
+        del result_window
+
+        screen.refresh()
+
+    except Exception as exc:
+
+        close_ui()
