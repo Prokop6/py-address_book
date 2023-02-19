63a082560707645ff933fb7f8511dcaf81718f42
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2023-02-19 20:50:32 +0100

    disabling button echo for program run duration

--- view/ui.py
+++ view/ui.py
@@ -5,6 +5,7 @@ def init_ui():
     """returns a new ncurses window"""
     new_screen = curses.initscr()
     curses.curs_set(False)
+    curses.noecho()
 
     return new_screen
 
@@ -12,5 +13,6 @@ def init_ui():
 def close_ui():
     """closes an ncurses window"""
 
+    curses.echo()
     curses.curs_set(True)
     curses.endwin()
